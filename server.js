a45a5a277dc9bae09e4fba1e4b9ff6d9630580e7
fabezio <fabezio@outlook.fr>
2020-10-16 21:33:53 +0200

    routes: about, signup & login

--- server.js
+++ server.js
@@ -6,8 +6,41 @@ const bodyParser = require('body-parser')
 const app = express()
 
 app.set("view engine", "ejs")
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended: false}))
+
+const db = "grandmasDB"
+
+mongoose.set("useUnifiedTopology", true)
+mongoose.connect(`mongodb://localhost/${db}`, {useNewUrlParser: true})
+
+// const Recipes = new mongoose.Schema({
+//     name: String,
+//     ingredients: Array
+// })
+
 
 app.route("/")
     .get((req, res) => res.render("index", {}))
+
+app.route("/login")
+    .get((req, res) => res.render("login", {}))
+
+app.route("/signup")
+    .get((req, res) => res.render("signup", {}))
+
+app.route("/about")
+    .get((req, res) => res.render("about", {}))
+
+// app.route("/forgot")
+//     .get((req, res) => res.render("forgot", {}))
+
+// app.route("/edit")
+//     .get((req, res) => res.render("edit", {}))
+
+// app.route("/newfavorite")
+//     .get((req, res) => res.render("newfavorite", {}))
+// app.route("/newfavorite")
+//     .get((req, res) => res.render("newfavorite", {}))
 
 app.listen(3000, () => console.log("serveur démarré sur port 3000"))
