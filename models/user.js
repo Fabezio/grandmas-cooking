fa87c9ec1eed1c9f10bf1bd0b77c07027e0b2085
fabezio <fabezio@outlook.fr>
2020-10-17 22:08:32 +0200

    ajout passport & express-session

--- models/user.js
+++ models/user.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
+const passportLocalStrategy = require('passport-local-mongoose')
 
 const userSchema = new mongoose.Schema({
   username: String,
   password: String
 })
+userSchema.plugin(passportLocalStrategy)
 
 module.exports = mongoose.model("User", userSchema)
--- server.js
+++ server.js
@@ -5,12 +5,23 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const flash = require('connect-flash')
 const bcrypt = require('bcrypt')
+const session = require('express-session')
+const passport = require('passport')
+const passportLocalStrategy = require('passport-local-mongoose')
 
 const app = express()
+app.use(session({
+    secret: "mysecret",
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: false}))
+
 
 const PORT = 3000
 const pw = "C0denCQRT"
@@ -19,7 +30,6 @@ const db = "cooking"
 // const userOne = coll_1
 // console.log(`collection principale: ${coll_1}`)
 // const atlasUrl = "mongodb+srv://fabezio:<password>@cluster0.0gg5s.mongodb.net/<dbname>?retryWrites=true&w=majority"
-const User = require("./models/user")
 const { base } = require('./models/user')
 
 
@@ -31,14 +41,11 @@ mongoose.connect(
         useNewUrlParser: true,
         useUnifiedTopology: true
     }
-)
-// mongoose.connect(`mongodb://localhost/${db}`, {useNewUrlParser: true})
-
-// const Recipes = new mongoose.Schema({
-//     name: String,
-//     ingredients: Array
-// })
-
+    )
+    const User = require("./models/user")
+passport.use(User.createStrategy())
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 app.route("/")
     .get((req, res) => res.render("index", {}))
@@ -81,25 +88,39 @@ app.route("/login")
     app.route("/signup")
     .get((req, res) => res.render("signup", {}))
     .post((req, res) => {
-        const saltRounds = 10
-        bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-            const user = {
-                username: req.body.username,
-                password: hash
+        // const saltRounds = 10
+        const newUser =  new User({
+            username: req.body.username,
+            // password: hash
+        })
+        User.register(newUser, req.body.password, (err, user) => {
+            if(err) {
+                console.log(err)
+                res.render("signup", {})
+            } else {
+                passport.authenticate("local")(req, res, function() {
+                    console.log(`Bienvenue, ${user.username}`)
+                    res.redirect("signup")
+
+                })
             }
-            // console.log(hash)
-            User.create(user, err => {
-                if (err) {
-                    console.log(err)
-                    res.send("un problème est survenu, recommence")
-                }
-                else {
-                    console.log(`Bienvenue, ${user.username}!`)
-                    res.render("index", {})
-                }
-            })
 
         })
+        // bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+        //     // else 
+        //     // console.log(hash)
+        //     User.create(user, err => {
+        //         if (err) {
+        //             console.log(err)
+        //             res.send("un problème est survenu, recommence")
+        //         }
+        //         else {
+        //             console.log(`Bienvenue, ${user.username}!`)
+        //             res.render("index", {})
+        //         }
+        //     })
+
+        // })
         // const user = {
             //     username: req.body.username,
         // console.log(user)
